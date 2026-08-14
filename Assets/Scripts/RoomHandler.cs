eb6fcf4a6caba0daca1f61b4af306dfc64039091
Charmeleon121 <45181933+Charmeleon121@users.noreply.github.com>
2026-08-14 19:36:02 +0100

    Basic room construction system in place

--- Assets/Scripts/RoomHandler.cs
+++ Assets/Scripts/RoomHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,8 +11,8 @@ public class RoomHandler : MonoBehaviour {
 	
 	// The highest valued ID currently in use - a new room would use this value + 1
 	private int currentMaxID;
-	
-	private List<GameObject[]> layers = new List<GameObject[]>();
+
+	private List<List<GameObject>> layers = new List<List<GameObject>>();
 	
 	// The room prefabs available
 	public GameObject normalRoomGhost;
@@ -30,11 +31,18 @@ public class RoomHandler : MonoBehaviour {
 		
 		currentMaxID = 0;
 		
-		GameObject[] startLayer = new GameObject[10];
-		startLayer[0] = GameObject.FindWithTag("Ladder");
+		List<GameObject> startLayer = new List<GameObject>();
+		startLayer.Add(GameObject.FindWithTag("Ladder"));
 		layers.Add(startLayer);
 	}
 	
+	/*
+	 * On FixedUpdate:
+	 * 
+	 * - Move the current ghost object in such a way that it snaps to nearby rooms for easy construction
+	 * - When there is a ghost object in play, and the player clicks, replace it with a new room at that position
+	 * - Ensure the new room has an incremented ID number, and that it's been added to the List of available rooms
+	 */
 	void FixedUpdate() {
 		GameObject activeGhost = GameObject.FindWithTag("Ghost");
 		
@@ -45,14 +53,20 @@ public class RoomHandler : MonoBehaviour {
 			Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
 			worldPos.x = 0f;
 			
-			GameObject closestRoom = FindClosestRoom(worldPos);
+			Tuple<GameObject, int> closestRoomData = FindClosestRoom(worldPos);
+			GameObject closestRoom = closestRoomData.Item1;
+			int roomYPos = closestRoomData.Item2;
+
 			Vector3 snapPosition;
-			if (worldPos.z <= closestRoom.transform.position.z) {
+			if (worldPos.z < closestRoom.transform.position.z) {
 				// Place to the right of the closest room
 				snapPosition = new Vector3(0f, closestRoom.transform.position.y, (closestRoom.transform.position.z - (closestRoom.transform.localScale.z * 2f)) - (activeGhost.transform.localScale.z * 2f));
-			} else {
+			} else if (worldPos.z > closestRoom.transform.position.z) {
 				// Place to the left of the closest room
 				snapPosition = new Vector3(0f, closestRoom.transform.position.y, (closestRoom.transform.position.z + (closestRoom.transform.localScale.z * 2f)) + (activeGhost.transform.localScale.z * 2f));
+			} else {
+				// Place beneath the closest room
+				snapPosition = new Vector3(0f, closestRoom.transform.position.y - 2f, closestRoom.transform.position.z);
 			}
 		
 			activeGhost.transform.position = snapPosition;
@@ -60,24 +74,17 @@ public class RoomHandler : MonoBehaviour {
 			if (clickAction.triggered) {
 				GameObject room = Instantiate(normalRoomPrefab, snapPosition, Quaternion.Euler(0f, 0f, 0f));
 				room.GetComponent<Room>().ID = currentMaxID + 1;
-				
-				int targetY = Mathf.RoundToInt(snapPosition.y + 1.25f);
-				
-				if (layers.Count < targetY) {
-					GameObject[] layer = new GameObject[10];
-					layer[0] = room;
+
+				if (layers.Count < roomYPos) {
+					List<GameObject> layer = new List<GameObject>();
+					layer.Add(room);
+					layers.Add(layer);
 				} else {
-					for (int i = 0; i < layers[targetY].Length; ++i) {
-						if (layers[targetY][i].GetType() == typeof(GameObject)) {
-							layers[targetY][i] = room;
-							break;
-						}
-					}
+					layers[roomYPos].Add(room);
 				}
 				
 				++currentMaxID;
-				Destroy(activeGhost.gameObject);
-				activeGhost = null;
+				Destroy(activeGhost);
 			}
 		}
 	}
@@ -95,13 +102,10 @@ public class RoomHandler : MonoBehaviour {
 		}
 	}
 
-	public void CreateRoom() {
-		Vector3 placementPos = GameObject.FindWithTag("Ghost Room").transform.position;
-	}
-
-	private GameObject FindClosestRoom(Vector3 position) {
+	private Tuple<GameObject, int> FindClosestRoom(Vector3 position) {
 		float minDistance = 1000f;
-		GameObject selected = null;
+		GameObject selectedRoom = null;
+		int selectedYLevel = -1;
 		
 		for (int yLevel = 0; yLevel < layers.Count; ++yLevel) {
 			foreach (GameObject room in layers[yLevel]) {
@@ -111,12 +115,13 @@ public class RoomHandler : MonoBehaviour {
 			
 					if (distNorm < minDistance) {
 						minDistance = distNorm;
-						selected = room;
+						selectedRoom = room;
+						selectedYLevel = yLevel;
 					}
 				}
 			}
 		}
 		
-		return selected; // TODO: Make this return the y level too - relying on ingame y pos is not sufficient
+		return new Tuple<GameObject, int>(selectedRoom, selectedYLevel);
 	}
 }
